b6fcbd0f6c893dcb7ccb1c1b0b410ed84e7a585d
Daniel Khabibullin <i6unny@gmail.com>
2023-07-03 16:47:24 +0500

    Add rework TodosTable, complete edit logic

---
+++ src/Components/Pages/Todos/TodosTable/TodoItem/TodoItem.tsx
@@ -0,0 +1,163 @@
+import {useEffect, useRef, useState} from "react";
+import {Button} from "react-bootstrap";
+import {useParams} from "react-router-dom";
+import {deleteTodo, updateTodo} from "../../../../../redux/action/todoActions";
+import {useAppDispatch} from "../../../../../redux/hooks";
+import {TodoType} from "../../../../../redux/reducer/todoReducer";
+
+interface TodoItemProps {
+	todo: TodoType;
+	index: number;
+};
+
+export const TodoItem: React.FC<TodoItemProps> = ({todo, index}) => {
+
+	const [isLoading, setIsLoading] = useState(false);
+	const dispatch = useAppDispatch();
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedText, setEditedText] = useState(todo.title);
+	const {userId} = useParams<{userId?: string}>();
+	const userIdNumber = userId ? parseInt(userId) : undefined;
+
+	const tdRef = useRef<HTMLTableCellElement>(null);
+
+	const handleDeleteTodo = async (todoId: number) => {
+		setIsLoading(true);
+		await dispatch<any>(deleteTodo(todoId));
+		setIsLoading(false);
+	};
+
+	const handleCompleteTodo = async (id: number, title: string, completed: boolean) => {
+		const payload: TodoType = {
+			id,
+			title,
+			completed: !completed,
+			userId: userIdNumber,
+		};
+		setIsLoading(true);
+		await dispatch<any>(updateTodo(payload));
+		setIsLoading(false);
+	};
+
+	const editTodo = () => {
+		setIsEditing(true);
+	};
+
+	const handleEditTodo = async (id: number, title: string, completed: boolean) => {
+		const payload: TodoType = {
+			id,
+			title: editedText,
+			completed,
+			userId: userIdNumber,
+		};
+		setIsEditing(false);
+		setIsLoading(true);
+		await dispatch<any>(updateTodo(payload));
+		setIsLoading(false);
+	};
+
+		useEffect(() => {
+		if (isEditing && tdRef.current) {
+			const tdElement = tdRef.current;
+			tdElement.focus();
+		}
+	}, [isEditing]);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				isEditing &&
+				tdRef.current &&
+				!tdRef.current.contains(event.target as Node)
+			) {
+				setIsEditing(false);
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [isEditing]);
+
+	return (
+		<tr
+			className={todo.completed ? 'table-success' : 'table-danger'}
+			key={todo.id}
+		>
+			<td>{index + 1}</td>
+			{isEditing ? 
+				<td
+					ref={tdRef}
+					contentEditable={true}
+					suppressContentEditableWarning={true}
+					onBlur={() => setEditedText(tdRef.current?.innerText || '')}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							e.preventDefault();
+							e.currentTarget.blur();
+							handleEditTodo(todo.id, todo.title, todo.completed)
+						}
+					}}
+				>
+					{editedText}
+				</td>
+				:
+				<td
+					className={`${todo.completed ?
+						'text-decoration-line-through table-success' : ''}`}
+					onDoubleClick={() => setIsEditing(true)}
+				>{todo.title}
+				</td>
+			}
+			<td>
+				<Button
+					variant='danger'
+					className='me-1 mb-1'
+					onClick={() => handleDeleteTodo(todo.id)}
+				>
+					Delete
+				</Button>
+				{!isEditing &&
+					<>
+						<Button
+							variant='primary'
+							className='me-1 mb-1'
+							onClick={editTodo}
+						>
+							Edit
+						</Button>
+						<Button
+							className='me-1 mb-1'
+							variant='secondary'
+							disabled={isLoading}
+							onClick={() => handleCompleteTodo(todo.id, todo.title, todo.completed)}
+						>
+							{todo.completed ? 'Incomplete' : 'Complete'}
+						</Button>
+					</>
+				}
+				{isEditing &&
+					<>
+						<Button
+							variant='success'
+							className='me-1 mb-1'
+							disabled={isLoading || editedText.trim().length === 0}
+							onClick={() => handleEditTodo(todo.id, todo.title, todo.completed)}
+						>
+							Save
+						</Button>
+						<Button
+							variant='secondary'
+							className='me-1 mb-1'
+							onClick={() => setIsEditing(false)}
+						>
+							Cancel
+						</Button>
+					</>
+				}
+			</td>
+		</tr>
+	);
+};
--- src/Components/Pages/Todos/TodosTable/TodosTable.tsx
+++ src/Components/Pages/Todos/TodosTable/TodosTable.tsx
@@ -1,42 +1,22 @@
-import {useState} from 'react';
-import {Button, Table} from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
-import {deleteTodo, updateTodo} from '../../../../redux/action/todoActions';
-
-import {useAppDispatch, useAppSelector} from '../../../../redux/hooks';
+import {Table} from 'react-bootstrap';
+import {Circles} from 'react-loader-spinner';
+import {useAppSelector} from '../../../../redux/hooks';
 import {TodoType} from '../../../../redux/reducer/todoReducer';
+import {TodoItem} from './TodoItem/TodoItem';
 
 export const TodosTable = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const dispatch = useAppDispatch();
-	const {userId} = useParams<{userId?: string}>();
-	const userIdNumber = userId ? parseInt(userId) : undefined;
-	console.log(isLoading);
 	const {todoList, loading} =
-		useAppSelector((state) => state.todo);
-	console.log(todoList);
+	useAppSelector((state) => state.todo);
 	console.log(loading);
-
-	const handleDeleteTodo = async (todoId: number) => {
-		setIsLoading(true);
-		await dispatch<any>(deleteTodo(todoId));
-		setIsLoading(false);
-	};
-
-	const handleEditTodo = async (id: number, title: string, completed: boolean) => {
-		const payload: TodoType = {
-			id,
-			title,
-			completed: !completed,
-			userId: userIdNumber,
-		};
-		setIsLoading(true);
-		await dispatch<any>(updateTodo(payload));
-		setIsLoading(false);
-	};
-
 	return (
 		<>
+		{loading ? (
+				<Circles
+					color='#0f7dc6'
+					height={200}
+					width={200}
+				/>
+			) : (
 			<Table striped bordered hover responsive>
 				<thead>
 					<tr>
@@ -46,33 +26,14 @@ export const TodosTable = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{todoList.map((todo, index) => (
-						<tr
-							className={todo.completed ? 'table-success' : 'table-danger'}
-							key={todo.id}
-						>
-							<td>{index + 1}</td>
-							<td>{todo.title}</td>
-							<td>
-								<Button
-									className='me-1 mb-1'
-									variant='secondary'
-									disabled={todo.completed}
-									onClick={() => handleEditTodo(todo.id, todo.title, todo.completed)}
-								>
-									Edit
-								</Button>
-								<Button
-									variant='danger'
-									className='me-1 mb-1'
-									onClick={() => handleDeleteTodo(todo.id)}
-								> Delete
-								</Button>
-							</td>
-						</tr>
-					))}
+					{todoList.map((todo: TodoType, index) => {
+						return (
+							<TodoItem key={todo.id} todo={todo} index={index}/>
+						);
+					})}
 				</tbody>
 			</Table>
+			)}
 		</>
 	);
 };
